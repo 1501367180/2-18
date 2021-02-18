18c11addd1cfe4f3b604b9fb0e698485d2f6e297
1501367180 <1501367180@qq.com>
2021-02-18 13:13:41 +0800

    2-18

---
+++ src/main/java/com/demo/demo218/Demo218Application.java
@@ -0,0 +1,13 @@
+package com.demo.demo218;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo218Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo218Application.class, args);
+    }
+
+}
